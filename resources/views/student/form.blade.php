8a2aee18b26b7fb8e826623661a25fbf9f4966da
Nuris Akbar <nuris.akbar@gmail.com>
2021-05-02 12:40:40 +0700

    init

--- resources/views/student/form.blade.php
+++ resources/views/student/form.blade.php
@@ -1,40 +1,102 @@
 <div class="row">
     <div class="col-md-6">
         <div class="row">
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label>NIPD</label>
-                        {!! Form::text('student_id', null, ['class'=>'form-control','placeholder'=>'NIPD']) !!}
-                        @if($errors->has('student_id'))
-                            <small id="emailHelp" class="form-text text-muted">{{ $errors->first('student_id') }}</small>
-                        @endif
-                    </div>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label>NIPD</label>
+                    {!! Form::text('student_id', null, ['class'=>'form-control','placeholder'=>'NIPD']) !!}
+                    @if($errors->has('student_id'))
+                        <small id="emailHelp" class="form-text text-muted">{{ $errors->first('student_id') }}</small>
+                    @endif
                 </div>
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label>NISN</label>
-                        {!! Form::text('student_id_second', null, ['class'=>'form-control','placeholder'=>'NISN']) !!}
-                        @if($errors->has('student_id_second'))
-                            <small id="emailHelp" class="form-text text-muted">{{ $errors->first('student_id') }}</small>
-                        @endif
-                    </div>
+            </div>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label>NISN</label>
+                    {!! Form::text('student_id_second', null, ['class'=>'form-control','placeholder'=>'NISN']) !!}
+                    @if($errors->has('student_id_second'))
+                        <small id="emailHelp" class="form-text text-muted">{{ $errors->first('student_id') }}</small>
+                    @endif
                 </div>
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label>Kelas</label>
-                        {!! Form::select('student_class_id', $class, null, ['class'=>'form-control']) !!}
-                        @if($errors->has('student_id'))
-                            <small id="emailHelp" class="form-text text-muted">{{ $errors->first('student_id') }}</small>
-                        @endif
-                    </div>
+            </div>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label>Kelas</label>
+                    {!! Form::select('student_class_id', $class, null, ['class'=>'form-control']) !!}
+                    @if($errors->has('student_id'))
+                        <small id="emailHelp" class="form-text text-muted">{{ $errors->first('student_id') }}</small>
+                    @endif
                 </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label>Tempat Lahir</label>
+                    {!! Form::text('born_place', null, ['class'=>'form-control','placeholder'=>'Tempat Lahir']) !!}
+                    @if($errors->has('born_place'))
+                        <small id="emailHelp" class="form-text text-muted">{{ $errors->first('born_place') }}</small>
+                    @endif
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label>Tanggal Lahir</label>
+                    {!! Form::date('born_date', null, ['class'=>'form-control','placeholder'=>'Tanggal Lahir']) !!}
+                    @if($errors->has('born_date'))
+                        <small id="emailHelp" class="form-text text-muted">{{ $errors->first('born_date') }}</small>
+                    @endif
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label>Agama</label>
+                    <select name="religion" class="form-control">
+                        @foreach($religions as $religion)
+                        <option value="{{$religion}}">{{$religion}}</option>
+                        @endforeach
+                    </select>
+                    @if($errors->has('born_date'))
+                        <small id="emailHelp" class="form-text text-muted">{{ $errors->first('born_date') }}</small>
+                    @endif
+                </div>
+            </div>
         </div>
     </div>
     <div class="col-md-6">
         <div class="row">
             <div class="col-md-6">
                 <div class="form-group">
-                    <label>Student Name</label>
+                    <label>Nama Siswa</label>
+                    {!! Form::text('name', null, ['class'=>'form-control','placeholder'=>'Student Name']) !!}
+                    @if($errors->has('name'))
+                        <small id="emailHelp" class="form-text text-muted">{{ $errors->first('name') }}</small>
+                    @endif
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label>NIK</label>
+                    {!! Form::text('name', null, ['class'=>'form-control','placeholder'=>'Student Name']) !!}
+                    @if($errors->has('name'))
+                        <small id="emailHelp" class="form-text text-muted">{{ $errors->first('name') }}</small>
+                    @endif
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label>Nama Siswa</label>
+                    {!! Form::text('name', null, ['class'=>'form-control','placeholder'=>'Student Name']) !!}
+                    @if($errors->has('name'))
+                        <small id="emailHelp" class="form-text text-muted">{{ $errors->first('name') }}</small>
+                    @endif
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label>NIK</label>
                     {!! Form::text('name', null, ['class'=>'form-control','placeholder'=>'Student Name']) !!}
                     @if($errors->has('name'))
                         <small id="emailHelp" class="form-text text-muted">{{ $errors->first('name') }}</small>
@@ -44,52 +106,6 @@
         </div>
     </div>
 </div>
-
-
-
-
-
-
-
-
-
-
-<div class="row">
-    <div class="col-md-2">
-        <div class="form-group">
-            <label>Tempat Lahir</label>
-            {!! Form::text('born_place', null, ['class'=>'form-control','placeholder'=>'Tempat Lahir']) !!}
-            @if($errors->has('born_place'))
-                <small id="emailHelp" class="form-text text-muted">{{ $errors->first('born_place') }}</small>
-            @endif
-        </div>
-    </div>
-    <div class="col-md-2">
-        <div class="form-group">
-            <label>Tanggal Lahir</label>
-            {!! Form::date('born_date', null, ['class'=>'form-control','placeholder'=>'Tanggal Lahir']) !!}
-            @if($errors->has('born_date'))
-                <small id="emailHelp" class="form-text text-muted">{{ $errors->first('born_date') }}</small>
-            @endif
-        </div>
-    </div>
-    <div class="col-md-2">
-        <div class="form-group">
-            <label>Agama</label>
-            <select name="religion" class="form-control">
-                @foreach($religions as $religion)
-                <option value="{{$religion}}">{{$religion}}</option>
-                @endforeach
-            </select>
-            @if($errors->has('born_date'))
-                <small id="emailHelp" class="form-text text-muted">{{ $errors->first('born_date') }}</small>
-            @endif
-        </div>
-    </div>
-</div>
-
-
-<hr>
 
 <div class="row">
     <div class="col-md-6"  style="border-right:1px dashed black;">
